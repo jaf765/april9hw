a8e55ace12923486874e46bcff991245caedf7c8
Julia Fernandez <jaf765@nyu.edu>
2020-04-14 13:03:44 -0400

    update server

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
-function button1() {
-   document.getElementById("fee").innerHTML = "there are " + document.querySelectorAll("p").length + " paragraph tags on this page";
-}
+const app = {
 
-function button2() {
-   document.getElementById("fi").innerHTML = "there are " + document.querySelectorAll("#demo p").length + " paragraph tags in the first ID";
-}
+  data: detailedBandObject, //this comes from my data.js files
 
-function button3() {
-  document.getElementById("fum").innerHTML = "there are " + document.querySelectorAll("#demo2 p").length + " paragraph tags in the second ID";
+  initialize: function () {
+    $('.box').click(function () {
+      $(this).toggleClass('transform')
+    });
+
+    $('.circle').dblclick(function () {
+      $(this).toggleClass('moveMe')
+    });
+
+    $('.listBeatles').click(function () {
+      app.listBeatles();
+    })
+
+    $('.compareBands').click(function () {
+      app.compareBands();
+    })
+  },
+
+  listBeatles: function () {
+    // can also this this.data, it means the same thing. app.data is safer
+    app.data.beatles.albums.forEach(album => {
+      $('.beatlesAlbums').append(`<li>${album.name}, ${album.year}`);
+    });
+  },
+
+  compareBands: function () {
+    const nirvanaMembers = app.data.nirvana.members;
+    const beatlesMembers = app.data.beatles.members;
+
+    $('.bandsComparison').text(`${nirvanaMembers.length > beatlesMembers.length ? 'Nirvana' : 'Beatles'} have more members`);
+  }
 }
